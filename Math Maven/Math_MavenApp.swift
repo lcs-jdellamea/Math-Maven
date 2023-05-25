5e494ced493f35694cbf7985b294e6fb57e695f0
Jack Dellamea <jdellamea@lcs.on.ca>
2023-05-25 15:18:36 -0400

    Initial Commit

---
+++ Math Maven/Math_MavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_MavenApp.swift
+//  Math Maven
+//
+//  Created by Jack Dellamea on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Math_MavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
